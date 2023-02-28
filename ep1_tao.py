e45a428ae899dbf1e138c9550cbe06f5f25a50c6
suthina <126534448+suthina@users.noreply.github.com>
2023-02-28 09:55:51 +0700

    แก้ไข hello และอัพโหลดโฟลเดอร์ package

--- ep1_tao.py
+++ ep1_tao.py
@@ -1,5 +1,10 @@
 Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+print('Hello world')
+print('Suthina')
+friend = ['Weerayut','White house','Leo']
+print(friend[0])
+
 >>> import turtle
 >>> tao = turtle.Pen()
 >>> tao.shape('turtle')
